f661df379a8939d60413e092fe2a488f3a5d3f8a
htetm <htetmyetaung.naing@ucalgary.ca>
2023-09-29 10:32:46 -0600

    added color mode

--- script.js
+++ script.js
@@ -7,11 +7,17 @@ const start = document.getElementById('start')
 const timerElement = document.getElementById('timer');
 const total = document.getElementById('totalTime');
 const gameElement = document.getElementById('game');
+const addFakeButtonCheckbox = document.getElementById('FakeButtonCheckbox');
+const checkboxElement = document.getElementById('checkboxContainer')
+const colorText = document.getElementById('colorText');
+let colorGame = false;
 let timerInterval;
 let rando = 0;
 let seconds = 0.0;
-
 let clickCount = 0;
+
+const colors = ["red", "green", "blue", "yellow", "pink", "purple", "orange"];
+
 
 function getRandomPosition() {
     const maxX = window.innerWidth - button.clientWidth;
@@ -26,13 +32,33 @@ function getRandomPosition() {
 function setRandomPosition() {
     if(clickCount < 10){
 
-            rando = Math.random() * 0.4 + 0.125;
-            gameplayAvg.textContent = `Average time: ${rando.toFixed(3)} seconds`;
+        rando = Math.random() * 0.4 + 0.125;
+        gameplayAvg.textContent = `Average time: ${rando.toFixed(3)} seconds`;
         
         const randomPosition = getRandomPosition();
+
+        if(colorGame){
+
+            const randomIndex = Math.floor(Math.random() * colors.length);
+            button.style.backgroundColor = colors[randomIndex];
+            colorText.textContent = `click ${colors[randomIndex].toUpperCase()}`
+
+            let fakerandomIndex = Math.floor(Math.random() * colors.length);
+            while(fakerandomIndex == randomIndex){
+                fakerandomIndex = Math.floor(Math.random() * colors.length);
+            }
+            fakeButton.style.backgroundColor = colors[fakerandomIndex];
+            button.style.backgroundColor = colors[randomIndex];
+            colorText.textContent = `click ${colors[randomIndex].toUpperCase()}`
+        }
+
         button.style.left = `${randomPosition.x}px`;
         button.style.top = `${randomPosition.y}px`;
+        const fakerandomPosition = getRandomPosition();
+        fakeButton.style.left = `${fakerandomPosition.x}px`;
+        fakeButton.style.top = `${fakerandomPosition.y}px`;
         clickCount++;
+        
     }
     else{
         button.style.display = 'none';
@@ -43,7 +69,6 @@ function setRandomPosition() {
         avgtime.textContent = `${rando.toFixed(3)} seconds`; 
     }
 }
-
 function startTimer() {
     timerElement.textContent = `Time: 0 seconds`;
     seconds = 0.0;
@@ -57,10 +82,23 @@ function stopTimer() {
     clearInterval(timerInterval);
 }
 
+
+function addFakeButton() {
+    fakeButton.style.display = 'block';
+    colorText.style.display = 'block';
+}
+
+function removeFakeButton() {
+    fakeButton.style.display = 'none';
+    colorText.style.display = 'none';
+}
+
 button.addEventListener('click', setRandomPosition);
 
+//Click Start button
 start.addEventListener('click', () => {
     gameElement.style.display = 'block';
+    checkboxElement.style.display ='none';
     button.style.display= 'block';
     start.style.display = 'none';
     clickCount = 0;
@@ -68,8 +106,20 @@ start.addEventListener('click', () => {
     setRandomPosition();
 });
 
+//Click restart button
 restart.addEventListener('click', () => {
     start.style.display = 'block';
+    checkboxElement.style.display ='block';
     winScreen.style.display = 'none';
 });
 
+addFakeButtonCheckbox.addEventListener('change', () => {
+    if (addFakeButtonCheckbox.checked) {
+        addFakeButton();
+        colorGame = true;
+
+    } else {
+        removeFakeButton();
+        colorGame = false;
+    }
+});
